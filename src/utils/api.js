1bd03790c20cd84a6ed8d8b46e4e6c7985463543
Moksha N <169030245+Moksh1704@users.noreply.github.com>
2026-04-02 20:12:09 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@
  */
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = 'https://universesocial.onrender.com';
 
 async function getAuthHeader() {
   try {
